d238ef3e98f6283b7c39357afc08ce1f09d47733
sfoyart <s.foyart@gmail.com>
2017-06-03 13:39:31 +0200

    UPD

--- WEB/firstsite/polls/models.py
+++ WEB/firstsite/polls/models.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-import datetime
 from __future__ import unicode_literals
-
+import datetime
 from django.db import models
 from django.utils import timezone
 # Create your models here.
